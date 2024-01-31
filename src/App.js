408db154990b01e29fee67ea29350de534631c76
Mindy D Tran <mindytran98@gmail.com>
2024-01-31 15:02:21 -0500

    add Nav component

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ import Currencies from "./pages/Currencies";
 import Main from "./pages/Main";
 import Price from "./pages/Price";
 
+//importing components
+import Nav from "./components/Nav";
+
 export default function App() {
   return (
     <div className="App">
+      <Nav />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/currencies" element={<Currencies />} />
