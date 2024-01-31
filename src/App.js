f46b10497f32af4afc3dd88d8cb41a5f6e3ace9e
Mindy D Tran <mindytran98@gmail.com>
2024-01-31 14:52:32 -0500

    copy code from lab

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
-export default function App(){
-    return(<h1>hello</h1>);
+// import "./App.css";
+
+//import route and our components
+import { Route, Routes } from "react-router-dom";
+import Currencies from "./pages/Currencies";
+import Main from "./pages/Main";
+import Price from "./pages/Price";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/currencies" element={<Currencies />} />
+        <Route path="/price" element={<Price />} />
+      </Routes>
+    </div>
+  );
 }
