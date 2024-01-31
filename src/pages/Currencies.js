92c9fd86ef78018250cf8f80d09dec3048592e78
Mindy D Tran <mindytran98@gmail.com>
2024-01-31 15:41:13 -0500

    copy code from lab

--- src/pages/Currencies.js
+++ src/pages/Currencies.js
@@ -1,3 +1,27 @@
-export default function Currencies() {
-  return <h1>This is the Currencies Component</h1>;
+import { Link } from "react-router-dom";
+
+export default function Currencies(props) {
+  const currencies = [
+    { name: "Bitcoin", symbol: "BTC" },
+    { name: "Litecoin", symbol: "LTC" },
+    { name: "Ethereum", symbol: "ETH" },
+    { name: "Ethereum Classic", symbol: "ETC" },
+    { name: "Stellar Lumens", symbol: "XLM" },
+    { name: "Dash", symbol: "DASH" },
+    { name: "Ripple", symbol: "XRP" },
+    { name: "Zcash", symbol: "ZEC" },
+  ];
+
+  return (
+    <div className="currencies">
+      {currencies.map((coin, i) => {
+        const { name, symbol } = coin;
+        return (
+          <Link key={i} to={`/price/${symbol}`}>
+            <h2>{name}</h2>
+          </Link>
+        );
+      })}
+    </div>
+  );
 }
